2d37ee9efdb89d5c6bd643d4c2478dfa6dd34a16
paddy <haslo to haslo>
2014-01-29 14:38:38 +0100

    Cleaning from warnings.

--- plant_care_app/src/main/java/com/platncare/app/activities/FeedActivity.java
+++ plant_care_app/src/main/java/com/platncare/app/activities/FeedActivity.java
@@ -1,22 +1,17 @@
 package com.platncare.app.activities;
 
 import android.app.Activity;
-;
+
 import android.os.Bundle;
 import com.platncare.app.R;
 import com.platncare.app.fragments.PlantsFeedFragment;
 import com.platncare.app.utils.FragmentUtils;
 import com.platncare.app.utils.IntentKeys;
-import model.Plant;
 import model.Token;
-
-import java.util.ArrayList;
 
 public class FeedActivity extends Activity {
 
-    private final static String LOG_TAG = FeedActivity.class.getSimpleName();
 
-    private PlantsFeedFragment plantsFeedFragment;
     private Token token;
 
     @Override
@@ -35,13 +30,13 @@ public class FeedActivity extends Activity {
             if(args.containsKey(IntentKeys.TOKEN_KEY)) {
                 token = (Token) args.getSerializable(IntentKeys.TOKEN_KEY);
             } else {
-                new RuntimeException("To initiate we need TokenKey.");
+               throw new RuntimeException("To initiate we need TokenKey.");
             }
         }
     }
 
     private void attachInitialFragment() {
-        plantsFeedFragment = PlantsFeedFragment.newInstance(token);
+        PlantsFeedFragment plantsFeedFragment = PlantsFeedFragment.newInstance(token);
         FragmentUtils.setFragment(this, plantsFeedFragment, R.id.frameLayoutContainer);
     }
 }
