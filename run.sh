de238b8627ddf2cffc74ccec6fa0e2c09e805e74
paddy <haslo to haslo>
2014-02-17 19:53:04 +0100

    Improving the starting animation for the app. Added starting theme for Launcher. Good read on that topic -> http://cyrilmottier.com/2013/01/23/android-app-launching-made-gorgeous/. Removed undeploy from the run scripts for easier debugging.

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 #mvn clean install -DskipTests
 mvn clean install
 pushd plant_care_app
-mvn -Pdebug-keystore,sign-debug android:undeploy android:deploy android:run
+#mvn -Pdebug-keystore,sign-debug android:undeploy android:deploy android:run
+mvn -Pdebug-keystore,sign-debug android:deploy android:run
 popd
